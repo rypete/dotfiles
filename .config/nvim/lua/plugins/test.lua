c246babb348b0c21b64ae096fbd2242c81bf5528
Ryan Peterson <ryan.peterson@nandos.co.uk>
2024-01-17 18:41:46 +0000

    add tmux, nvim

---
+++ .config/nvim/lua/plugins/test.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    enabled = false,
+    dependencies = {
+      "haydenmeade/neotest-jest",
+      "marilari88/neotest-vitest",
+    },
+    keys = {
+      {
+        "<leader>tl",
+        function()
+          require("neotest").run.run_last()
+        end,
+        desc = "Run Last Test",
+      },
+      {
+        "<leader>tw",
+        "<cmd>lua require('neotest').run.run({ jestCommand = 'jest --watch ' })<cr>",
+        desc = "Run Watch",
+      },
+    },
+    opts = function(_, opts)
+      table.insert(
+        opts.adapters,
+        require("neotest-jest")({
+          jestCommand = "yarn test",
+          jestConfigFile = "jest.config.ts",
+          env = { CI = true },
+          cwd = function()
+            return vim.fn.getcwd()
+          end,
+        })
+      )
+      table.insert(opts.adapters, require("neotest-vitest"))
+    end,
+  },
+}
